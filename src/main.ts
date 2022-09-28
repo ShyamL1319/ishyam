14bb6ab04f96ec4f7ddc2a8223e24e14454785d1
shyamlal98 <shyaml.cse16@gmail.com>
2022-09-28 23:45:15 +0530

    addedd swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './shared/logging.interceptor';
+import { createdocument } from './swagger/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -8,8 +11,9 @@ async function bootstrap() {
       transform: true,
     })
   )
-  console.log(process.env)
-  app.setGlobalPrefix('api/v1');
+  SwaggerModule.setup('api', app, createdocument(app));
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  // app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
 bootstrap();
